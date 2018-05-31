4b5e7d4beb08d138a2f38a83066832e3c4de92bb
Kartik <Kartik@LAPTOP-B14HR2LL>
2018-05-31 12:55:15 -0400

    Project Added

---
+++ E_Commerce_Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Commerce_Site.Startup))]
+namespace E_Commerce_Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
